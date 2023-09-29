dfdb0e9c0737efd79a7db20b99e6114cbf3420b0
Alesvoy <alesvoy@gmail.com>
2023-09-29 12:28:29 -0600

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import PokerCard from 'react-pokercards';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Hello World</h1>
+     <PokerCard short='As'/>
+    </>
+  )
+}
+
+export default App
